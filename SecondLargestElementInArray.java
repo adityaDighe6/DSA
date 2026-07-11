3983659fdebb5d84d6d1a0bf34315b827516f5ab
adityaDighe6 <aditya.mdighe@gmail.com>
2026-07-11 08:56:28 +0530

    Add files via upload

---
+++ SecondLargestElementInArray.java
@@ -0,0 +1,45 @@
+package DSAPractice;
+
+import java.util.Arrays;
+
+public class SecondLargestElementInArray {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int arr[] = {55, 10, 8, 90, 43, 87, 95, 25, 50, 12};
+		Aditya aditya = new Aditya();
+		
+		//Approach One
+		aditya.secondLargestApproachOne(arr);
+		//End
+		
+		//Approach Two
+		int result = aditya.secondLargestApproachTwo(arr);
+		System.out.println("Second largest element is : "+result);
+		//End
+	}
+}
+
+class Aditya{
+	 //Approach 1 : Using Arrays.sort
+	 public int secondLargestApproachOne(int arr[]) { 
+		 Arrays.toString(arr); Arrays.sort(arr);
+		 System.out.println("Second largest element is : "+arr[arr.length-2]);
+		 return arr[arr.length-2]; 
+	  }
+	 //End
+	 
+	 //Approach 2 : Using nested for loops
+	  int a;
+	  public int secondLargestApproachTwo(int arr[]) { 
+		  for(int i = 0; i<arr.length; i++) {//traversing array
+			  for(int j = i+1; j<arr.length; j++) {//comparison
+				  if(arr[i] > arr[j]) { 
+					  a = arr[i]; 
+					  arr[i] = arr[j]; 
+					  arr[j] = a; } 
+				  } 
+			  }
+	  return arr[arr.length-2]; 
+	  }
+}
